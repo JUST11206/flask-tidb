df7aadf1c969968e4db18a628e78e688dd9ffb29
JUST11206 <malikshoaib7983@gmail.com>
2026-06-13 17:28:54 +0530

    Add mail debug logs

--- app.py
+++ app.py
@@ -46,7 +46,8 @@ app.config.update(
     MAIL_USE_SSL=False,
     MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
     MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
-    MAIL_DEFAULT_SENDER=os.getenv("MAIL_USERNAME")
+    MAIL_DEFAULT_SENDER=os.getenv("MAIL_USERNAME"),
+     MAIL_TIMEOUT=20
 )
 
 mail = Mail(app)
